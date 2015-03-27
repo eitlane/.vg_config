3bcb703dbd10a83600b439d408a532dbe678155c
Vencislav GEORGIEV <vgeorgiev@amadeus.com>
2015-03-27 11:55:48 +0100

    aded top RC

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-rm -rf ~/.vim ~/.vimrc  ~/.bashrc
+rm -rf ~/.vim ~/.vimrc  ~/.bashrc ~/.toprc
 ln -s  ~/.vg_config/bashrc ~/.bashrc
 ln -s  ~/.vg_config/vim    ~/.vim
 ln -s  ~/.vg_config/vimrc  ~/.vimrc
+ln -s  ~/.vg_config/toprc  ~/.toprc
