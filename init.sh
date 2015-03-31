65a6df5c3720940dd1986d85bb36e6d5315c3d5f
Your Name <you@example.com>
2015-03-31 14:54:41 +0200

    Added init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,19 @@
+echo "Spawining vim bash and top configuration"
+
 rm -rf ~/.vim ~/.vimrc  ~/.bashrc ~/.toprc
 ln -s  ~/.vg_config/bashrc ~/.bashrc
 ln -s  ~/.vg_config/vim    ~/.vim
 ln -s  ~/.vg_config/vimrc  ~/.vimrc
 ln -s  ~/.vg_config/toprc  ~/.toprc
+
+echo "Done!"
+
+echo "Getting plugins from git"
+
+/bin/sh  ~/.vg_config/vim/bundle/ctrlp.sh
+/bin/sh  ~/.vg_config/vim/bundle/nerdtree.sh
+/bin/sh  ~/.vg_config/vim/bundle/rename.sh
+/bin/sh  ~/.vg_config/vim/bundle/tagbar.sh
+/bin/sh  ~/.vg_config/vim/bundle/vim-air.sh
+
+echo "Done!"
